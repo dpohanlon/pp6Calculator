3604c508fbb48bb61678e2b39e396ac2d80ea93b
Daniel O'Hanlon <dpohanlon@gmail.com>
2014-03-17 15:23:55 +0000

    Added arithmetic and streaming operators.

--- src/pp6Utils/Vector4.hpp
+++ src/pp6Utils/Vector4.hpp
@@ -4,6 +4,7 @@
 #include <cmath>
 
 #include "myMath.hpp"
+#include <iostream>
 
 class Vector4
 {
@@ -29,11 +30,18 @@ public:
     bool setY(double val);
     bool setZ(double val);
 
-    // Boost in z-direction by velocity v
+    // Boost in alon z-axis by velocity v
     bool boostZ(double v);
 
     // Returns 4-vector magnitude using g = diag(-1, 1, 1, 1)
     double mag(void) const;
+
+    Vector4 & operator += (const Vector4 & v) { m_t += v.getT(); m_x += v.getX(); m_y += v.getY(); m_z += v.getZ(); return *this; }
+    Vector4 & operator -= (const Vector4 & v) { m_t -= v.getT(); m_x -= v.getX(); m_y -= v.getY(); m_z -= v.getZ(); return *this; }
+    Vector4 & operator *= (const Vector4 & v) { m_t *= v.getT(); m_x *= v.getX(); m_y *= v.getY(); m_z *= v.getZ(); return *this; }
+    Vector4 & operator /= (const Vector4 & v) { m_t /= v.getT(); m_x /= v.getX(); m_y /= v.getY(); m_z /= v.getZ(); return *this; }
+
+    Vector4 operator - (void) { return Vector4(-m_t, -m_x, -m_y, -m_z); }
 
 private:
 
@@ -48,5 +56,44 @@ private:
     causalType interval(void) const;
 
 };
+
+inline Vector4 operator + (const Vector4 & v, const double c) { return Vector4(v.getT() + c, v.getX() + c, v.getY() + c, v.getZ() + c); }
+inline Vector4 operator - (const Vector4 & v, const double c) { return Vector4(v.getT() - c, v.getX() - c, v.getY() - c, v.getZ() - c); }
+
+inline Vector4 operator + (const double c, const Vector4 & v) { return Vector4(c + v.getT(), c + v.getX(), c + v.getY(), c + v.getZ()); }
+inline Vector4 operator - (const double c, const Vector4 & v) { return Vector4(c - v.getT(), c - v.getX(), c - v.getY(), c - v.getZ()); }
+
+inline Vector4 operator + (const Vector4 & v1, const Vector4 & v2) { return Vector4(v1.getT() + v2.getT(), v1.getX() + v2.getX(), v1.getY() + v2.getY(), v1.getY() + v2.getY()); }
+inline Vector4 operator - (const Vector4 & v1, Vector4 & v2) { return v1 + (-v2); } // Second arg not const?
+
+inline std::ostream& operator<<(std::ostream & stream, const Vector4 & v)
+{
+    stream << "[ ";
+    stream << v.getT() << " ";
+    stream << v.getX() << " ";
+    stream << v.getY() << " ";
+    stream << v.getZ();
+
+    return stream;
+}
+
+inline std::istream& operator>>(std::istream & stream, Vector4 & v)
+{
+    double t = 0;
+    double x = 0;
+    double y = 0;
+    double z = 0;
+
+    stream >> t >> x >> y >> z;
+
+    v.setT(t);
+    v.setX(x);
+    v.setY(y);
+    v.setZ(z);
+
+    return stream;
+}
+
+
 
 #endif
